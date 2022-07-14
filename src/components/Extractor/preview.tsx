0544b932b562e60f51aad90ed59bda4f0a7118f1
Luan Dantas <luandr92@gmail.com>
2022-07-14 17:01:30 -0300

    Begin to implement pdf preview

--- src/components/Extractor/preview.tsx
+++ src/components/Extractor/preview.tsx
@@ -1,4 +1,8 @@
+import ReactCrop, { Crop } from 'react-image-crop'
 import { PreviewBlank } from "./preview-blank";
+import { useEffect, useState } from 'react';
+
+import 'react-image-crop/dist/ReactCrop.css'
 
 interface PreviewProps {
   selectedFile: any;
@@ -6,12 +10,24 @@ interface PreviewProps {
 }
 
 export function Preview({ selectedFile, preview }: PreviewProps) {
+  const [crop, setCrop] = useState<Crop>()
+
+  useEffect(() => {
+    console.log(crop)
+  }, [crop])
+
   return (
     <>
       {selectedFile ? (
-        <div>
-          <img src={preview} className="w-auto" />
-        </div>
+        <>
+          {selectedFile.type === 'application/pdf' ? (
+            {preview}
+          ) : (
+            <ReactCrop crop={crop} onChange={c => setCrop(c)}>
+              <img src={preview} />
+            </ReactCrop>
+          )}
+        </>
       ) : (
         <PreviewBlank />
       )}

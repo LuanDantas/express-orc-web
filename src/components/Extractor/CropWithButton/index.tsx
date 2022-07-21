caa3a0910206ad40f06c7925ebdb240a3c76f42a
Luan Dantas <luandr92@gmail.com>
2022-07-21 15:57:53 -0300

    Apply styles changes

--- src/components/Extractor/CropWithButton/index.tsx
+++ src/components/Extractor/CropWithButton/index.tsx
@@ -22,7 +22,7 @@ export function CropWithButton({ image }: CropProps) {
             </button>
           }
           deleteButton={
-            <button type="button" className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="button" className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-rose-400 hover:bg-rose-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               <TrashIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
               Deletar seleção
             </button>

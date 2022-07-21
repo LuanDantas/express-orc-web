ebd7366b3b4a7922af3fdaf3209f3f021d7ef97b
Luan Dantas <luandr92@gmail.com>
2022-07-21 16:43:26 -0300

    Apply styles changes

--- src/components/Extractor/index.tsx
+++ src/components/Extractor/index.tsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Preview } from "./Preview";
+import Result from "./Results";
 import { Upload } from "./Upload";
 
 export function Extractor() {
   const [selectedFile, setSelectedFile] = useState()
   const [preview, setPreview] = useState<string | undefined>()
+  const [openResults, setOpenResults] = useState(false)
 
   const theme = 'buttons' // default || buttons
+  
+  useEffect(() => {
+    setOpenResults(true);
+  }, [])
 
   return (
     <section className="flex justify-between h-5/6 mt-2 px-5">
-      <div className="flex w-2/4 pr-3">
+      <div className="flex w-2/4 pr-3 flex-wrap">
         <Upload
           selectedFile={selectedFile}
           setSelectedFile={setSelectedFile}
@@ -20,6 +26,8 @@ export function Extractor() {
       <div className="flex w-2/4 pl-3">
         <Preview selectedFile={selectedFile} preview={preview} theme={theme} />
       </div>
+
+      <Result onOpenResults={setOpenResults} showResults={false} />
     </section>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,6 +11,7 @@ import {
   PencilIcon,
 } from '@heroicons/react/solid'
 import { Menu, Transition } from '@headlessui/react'
+import SelectTheme from '../Extractor/SelectTheme'
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
@@ -23,15 +24,7 @@ export function Header() {
         <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Express OCR Web</h2>
       </div>
       <div className="mt-5 flex lg:mt-0 lg:ml-4">
-        <span className="hidden sm:block">
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <PencilIcon className="-ml-1 mr-2 h-5 w-5 text-gray-500" aria-hidden="true" />
-            Editar
-          </button>
-        </span>
+        <SelectTheme />
 
         <span className="sm:ml-3">
           <button
@@ -43,7 +36,6 @@ export function Header() {
           </button>
         </span>
 
-        {/* Dropdown */}
         <Menu as="div" className="ml-3 relative sm:hidden">
           <Menu.Button className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Mais
